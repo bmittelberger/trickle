caa6117036f4e8e42b75cf977c8bc763d800ebfb
Ben Mittelberger <bmittelb@stanford.edu>
2016-02-29 01:16:03 -0800

    added some create ish

--- trickle/trickle/UserCreateViewController.swift
+++ trickle/trickle/UserCreateViewController.swift
@@ -16,10 +16,13 @@ class UserCreateViewController: UIViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var venmoPhoneTextField: UITextField!
     
+    static var currentPassword = ""
+    
     @IBOutlet weak var createButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         // Do any additional setup after loading the view.
     }
     
@@ -40,6 +43,44 @@ class UserCreateViewController: UIViewController {
     
 
     @IBAction func doCreate(sender: UIButton) {
+        let regColor : UIColor = UIColor( red: 1.0, green:  1.0, blue: 1.0, alpha: 0.0)
+        emailTextField.backgroundColor = regColor
+        passwordTextField.backgroundColor = regColor
+        firstNameTextField.backgroundColor = regColor
+        lastNameTextField.backgroundColor = regColor
+        venmoPhoneTextField.backgroundColor = regColor
+        var anyBadInputs : Bool = false
+        let errorColor : UIColor = UIColor( red: 231/255.0, green: 76/255.0, blue:60/255.0, alpha: 0.3 )
+        if "" == emailTextField.text {
+            emailTextField.backgroundColor = errorColor
+            anyBadInputs = true
+        }
+        if "" == passwordTextField.text {
+            passwordTextField.backgroundColor = errorColor
+            anyBadInputs = true
+        }
+        if "" == firstNameTextField.text {
+            firstNameTextField.backgroundColor = errorColor
+            anyBadInputs = true
+        }
+        if "" == lastNameTextField.text {
+            lastNameTextField.backgroundColor = errorColor
+            anyBadInputs = true
+        }
+        if "" == venmoPhoneTextField.text {
+            venmoPhoneTextField.backgroundColor = errorColor
+            anyBadInputs = true
+        }
+        if anyBadInputs {
+            Error.show("Please fix highlighted fields.", location:  self)
+            return
+        }
+        
+        
+        User.me.email = emailTextField.text!
+        UserCreateViewController.currentPassword = passwordTextField.text!
+        User.me.first = firstNameTextField.text!
+        User.me.last = lastNameTextField.text!
         
     }
     /*
